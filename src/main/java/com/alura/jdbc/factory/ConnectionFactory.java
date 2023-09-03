090785aa771345a0ae3d9431bd8884142ce1eae3
Felix <felixnicolas-m@hotmail.com>
2023-09-03 14:22:00 -0300

    Pool de conexiones con datasource

--- src/main/java/com/alura/jdbc/factory/ConnectionFactory.java
+++ src/main/java/com/alura/jdbc/factory/ConnectionFactory.java
@@ -4,13 +4,31 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import javax.management.loading.PrivateClassLoader;
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 public class ConnectionFactory {
 	
-	public Connection recuperaConexion() throws SQLException {
+	private DataSource datasource;
+	
+	public ConnectionFactory() {	
 		
-		return DriverManager.getConnection("jdbc:mysql://localhost/control_de_stock?useTimeZone=true&serverTimeZone=UTC", "root", "root1234");
+		var pooledDataSource = new ComboPooledDataSource();
 		
+		pooledDataSource.setJdbcUrl("jdbc:mysql://localhost/control_de_stock?useTimeZone=true&serverTimeZone=UTC");
+		pooledDataSource.setUser("root");
+		pooledDataSource.setPassword("root1234");
+		
+		this.datasource = pooledDataSource;
 	}
+	
+	public Connection recuperaConexion() throws SQLException{
+		return this.datasource.getConnection();
+	}
+	
+
 	
 
 }
