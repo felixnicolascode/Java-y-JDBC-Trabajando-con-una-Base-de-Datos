51e04b0078da71f18b4b806f3692194c3e8a18db
Felix <felixnicolas-m@hotmail.com>
2023-09-03 14:32:20 -0300

    Prueba Pool de Conexiones

--- src/main/java/com/alura/jdbc/factory/ConnectionFactory.java
+++ src/main/java/com/alura/jdbc/factory/ConnectionFactory.java
@@ -20,6 +20,7 @@ public class ConnectionFactory {
 		pooledDataSource.setJdbcUrl("jdbc:mysql://localhost/control_de_stock?useTimeZone=true&serverTimeZone=UTC");
 		pooledDataSource.setUser("root");
 		pooledDataSource.setPassword("root1234");
+		pooledDataSource.setMaxPoolSize(10);
 		
 		this.datasource = pooledDataSource;
 	}
